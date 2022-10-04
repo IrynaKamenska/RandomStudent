8e150fd45ad4b7b4ec9a50b128d2bc09e5c80ae1
Iryna <pshonka@gmail.com>
2022-10-04 15:44:47 +0200

    bugfixes in StudentDB and unit tests

--- src/main/java/de/bootcamp/StudentDB.java
+++ src/main/java/de/bootcamp/StudentDB.java
@@ -1,10 +1,12 @@
 package de.bootcamp;
 
+import java.rmi.server.ExportException;
+
 public class StudentDB {
     private Student[] students;
 
     public StudentDB(Student [] students){
-        this.students = new Student[]{};
+        this.students = students;
     }
     public  Student[] getAllStudents(){
         return students;
@@ -20,6 +22,15 @@ public class StudentDB {
             int studentId = student.getId();
             if(id == studentId){
                 return student;
+            }
+        }
+        throw new RuntimeException("Student mit id: " + id + "wurde nicht gefunden");
+    }
+
+    public Student findStudentById(int id) throws Exception {
+        for (int i = 0; i < students.length ; i++) {
+            if(id == students[i].getId()){
+                return students[i];
             }
         }
         throw new RuntimeException("Student mit id: " + id + "wurde nicht gefunden");
--- src/test/java/de/bootcamp/StudentDBTest.java
+++ src/test/java/de/bootcamp/StudentDBTest.java
@@ -35,16 +35,43 @@ class StudentDBTest {
 
         //when
         Student[] actual = db.getAllStudents();
-        Student[] expected = new Student[]{student1};
+        Student[] expected = new Student[]{student1, student2};
         //then
         assertArrayEquals(expected,actual);
     }
 
     @Test
-    void findById() {
+    public void findById() throws Exception {
         //given
-        Student student1 = new Student("Zeshan",1);
-        Student student2 = new Student("Ana",2);
+        Student student1 = new Student("Zeshan",0);
+        Student student2 = new Student("Ana",1);
+        Student [] array1 = new Student[2];
+        array1[0] = student1;
+        array1[1] = student2;
+
+        StudentDB db = new StudentDB(array1);
+
+//
+//        for (int i = 0; i < array1.length; i++) {
+//            System.out.println(array1[i]);
+//        }
+
+
+       // when
+       Student actual = db.findStudentById(1);
+       Student expected = (array1[1]);
+
+       // then
+
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void findByIdWithTryCatch() throws Exception {
+        //given
+        Student student1 = new Student("Zeshan",0);
+        Student student2 = new Student("Ana",1);
         Student [] array1 = new Student[2];
         array1[0] = student1;
         array1[1] = student2;
@@ -52,17 +79,18 @@ class StudentDBTest {
         StudentDB db = new StudentDB(array1);
 
 
-        for (int i = 0; i < array1.length; i++) {
-            System.out.println(array1[i]);
+        // when
+        try{
+            db.findStudentById(2);
+            fail("Exception ist nicht aufgetreten");
+
+        } catch(Exception e){
+            System.out.println("Exception ist aufgetreten");
         }
 
 
-       // when
-       Student actual = db.findById(2);
-
-       // then
-        Student expected = student2;
-        assertEquals(expected, actual);
-
     }
+
+
+
 }
